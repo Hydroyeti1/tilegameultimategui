9422341aa43e184c743d59efb7dcb2ed3f0b7c94
MegaKonami <86085189+MegaKonami@users.noreply.github.com>
2022-08-11 19:17:34 -0500

    Update squidgame.lua

--- squidgame.lua
+++ squidgame.lua
@@ -1,9 +1,13 @@
+-- Using Kavo UI --
+-- All other scripting by Hydroyeti and Colonel_Konami/MegaKonami --
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Anticheat Moment", "Synapse")
 local Tab = Window:NewTab("Fun")
 local Section = Tab:NewSection("Fun")
 local End = game:GetService("Workspace").End.EndDoor.EndDoorTeleport
+local Remote_PlayerNewPane = game:GetService("ReplicatedStorage").Remotes.PlayerNewPane
 T1 = false
+T2 = false
 Section:NewToggle("AutoFarm", "Farms winz", function(state)
   T1 = state
 end)
@@ -21,9 +25,19 @@ Section:NewButton("Jesus", "Panels cannot break.", function(state)
     end
   end
 end)
+Section:NewToggle("Fast Body Pickup", "Instantly picks up bodies.", function(state)
+  T2 = state
+end)
 
 game:GetService("RunService").RenderStepped:Connect(function()
   if T1 then
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = End.CFrame
+  end
+  for _,v in pairs(game:GetService("Workspace").Clones:GetChildren()) do
+    if T2 then
+      v.HumanoidRootPart.Prompt.HoldDuration = 0
+    else
+      v.HumanoidRootPart.Prompt.HoldDuration = 0.75
+    end
   end
 end)
